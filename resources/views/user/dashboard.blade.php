39d3205f10dddd08f7203771c0fc81d5eacc2b99
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-08-17 17:04:03 +0530

    dfgh

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -9,24 +9,34 @@
                 <div class="left-side">
 
                     <div class="title text-center">
-                        <p translate="no">Mining <span>panel</span></p>
+                        <p translate="no">Your Referral Link</p>
                     </div>
 
+
+
+                    <div class="address">
+
+<input class="def-input" type="text" value="{{ asset('') }}register?ref={{ Auth::user()->username }}" id="copied_text" readonly style=" width: 566px;">
+<i class="fa-solid fa-copy" onclick="CopiedText()"></i>
+
+</div>
+
+</br></br>
                     <div class="main-balance">
                         <div>
                             <img src="{{ asset('') }}upnl/assets/img/account/main_balance.png" alt="balance for withdrawal">
-                            <p translate="no">0.00<span>TRX</span></p>
+                            <p translate="no">{{ number_format(Auth::user()->withdraw(), 2) }}{{currency()}}<span>TOTAl WITHDRAWAL</span></p>
                         </div>
                         <div>
                             <a href="{{route('user.withdraw')}}" class="def-btn yellow-btn"><i
                                     class="fa-solid fa-minus"></i></a>
                         </div>
                     </div>
-
+<!-- 
                     <div class="accumulated-income">
                         <img src="{{ asset('') }}upnl/assets/img/account/tron.png" alt="accumulated-income">
                         <span translate="no" class="profit" id="profit" data-prc="5.787037037037E-6" data-tm="1059"
-                            data-sum="0">0.0061284</span> </div>
+                            data-sum="0">0.0061284</span> </div> -->
 
                     <div class="collect-or-increase">
                         <form action="/dashboard" method="post" class="income-collection">
@@ -52,14 +62,14 @@
 
                     <div class="statistics">
                         <p>
-                            <span>PARTICIPANTS HAVE EARNED:</span>
-                            <span translate="no"><span>{{ number_format((\App\Models\Income::sum('comm')), 2) }}
-                            </span>{{currency()}}</span>
+                            <span>ALL GLOBAL PARTICIPANTS:</span>
+                            <span translate="no"><span>{{ $clubA }}
+                            <!-- </span>{{currency()}}</span> -->
                         </p>
                         <p>
-                            <span>TOTAL DIRECT:</span>
-                            <span translate="no"><span>{{$user_direct}}
-                                    </span></span>
+                            <span>PARTICIPANTS HAVE EARNED:</span>
+                            <span translate="no"><span>{{ number_format((\App\Models\Income::sum('comm')), 2) }}
+                                    </span>{{currency()}}</span>
                         </p>
                     </div>
 
@@ -70,26 +80,23 @@
                                 translate="no"><span>{{$total_team}}</span></span>
                         </p>
                         <p>
-                            <span>TEAM BUILD INCOME:</span>
+                            <span>TOTAL DIRECT:</span>
                             <span
-                                translate="no"><span>{{ number_format(Auth::user()->level_bonus->sum('comm'), 2) }}</span>{{currency()}}</span>
+                                translate="no"><span>{{$user_direct}}</span></span>
                         </p>
                     </div>
 
                     <div class="statistics">
                         <p>
-                            <span>GLOBAL INCOME </span>
+                            <span>TEAM BUILD INCOME </span>
                             <span
-                                translate="no"><span>{{ number_format(Auth::user()->pool_bonus->sum('comm'), 2) }}</span>{{currency()}}</span>
+                                translate="no"><span>{{ number_format(Auth::user()->level_bonus->sum('comm'), 2) }}</span>{{currency()}}</span>
                         </p>
                         <p>
-                <span>TOTAl WITHDRAWAL:</span>
-                <span translate="no">{{ number_format(Auth::user()->withdraw(), 2) }} <span>{{currency()}}</span></span>
+                <span>GLOBAL INCOME:</span>
+                <span translate="no">{{ number_format(Auth::user()->pool_bonus->sum('comm'), 2) }} <span>{{currency()}}</span></span>
             </p>
-            <p>
-                <span>Profit per month:</span>
-                <span translate="no">15.00<span>TRX</span></span>
-            </p>
+           
                     </div>
 
                 </div>
